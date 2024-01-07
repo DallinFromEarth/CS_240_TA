bff86936c48797ecd257a74d210c6a65ed937d4d
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-01-06 20:48:21 -0700

    Restart - Clean blank slate
    Course provided README.md got renamed to ProjectOverview.md. README.md is now a log of problems and solutions I encounter

--- client/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import chess.*;
-
-public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-    }
-}
