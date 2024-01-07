6e8cb0fc6b2c2ed30d2a627d17af7e69812a2eff
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-01-06 20:11:41 -0700

    first commit - set up - lots broken

---
+++ README.md
@@ -0,0 +1,40 @@
+# ♕ BYU CS 240 Chess
+
+This project demonstrates mastery of proper software design, client/server architecture, networking using HTTP and WebSocket, database persistence, unit testing, serialization, and security.
+
+## 10k Architecture Overview
+
+The application implements a multiplayer chess server and a command line chess client.
+
+[![Sequence Diagram](10k-architecture.png)](https://sequencediagram.org/index.html#initialData=C4S2BsFMAIGEAtIGckCh0AcCGAnUBjEbAO2DnBElIEZVs8RCSzYKrgAmO3AorU6AGVIOAG4jUAEyzAsAIyxIYAERnzFkdKgrFIuaKlaUa0ALQA+ISPE4AXNABWAexDFoAcywBbTcLEizS1VZBSVbbVc9HGgnADNYiN19QzZSDkCrfztHFzdPH1Q-Gwzg9TDEqJj4iuSjdmoMopF7LywAaxgvJ3FC6wCLaFLQyHCdSriEseSm6NMBurT7AFcMaWAYOSdcSRTjTka+7NaO6C6emZK1YdHI-Qma6N6ss3nU4Gpl1ZkNrZwdhfeByy9hwyBA7mIT2KAyGGhuSWi9wuc0sAI49nyMG6ElQQA)
+
+## IntelliJ Support
+
+Open the project directory in IntelliJ in order to develop, run, and debug your code using an IDE.
+
+## Maven Support
+
+You can use the following commands to build, test, package, and run your code.
+
+| Command                    | Description                                     |
+| -------------------------- | ----------------------------------------------- |
+| `mvn compile`              | Builds the code                                 |
+| `mvn package`              | Run the tests and build an Uber jar file        |
+| `mvn package -DskipTests`  | Build an Uber jar file                          |
+| `mvn install`              | Installs the packages into the local repository |
+| `mvn test`                 | Run all the tests                               |
+| `mvn -pl shared tests`     | Run all the shared tests                        |
+| `mvn -pl client exec:java` | Build and run the client `Main`                 |
+| `mvn -pl server exec:java` | Build and run the server `Main`                 |
+
+These commands are configured by the `pom.xml` (Project Object Model) files. There is a POM file in the root of the project, and one in each of the modules. The root POM defines any global dependencies and references the module POM files.
+
+### Running the program using Java
+
+Once you have compiled your project into an uber jar, you can execute it with the following command.
+
+```sh
+java -jar client/target/client-jar-with-dependencies.jar
+
+♕ 240 Chess Client: chess.ChessPiece@7852e922
+```
---
+++ shared/src/main/chess/ChessGame.java
@@ -0,0 +1,110 @@
+package chess;
+
+import java.util.Collection;
+
+/**
+ * For a class that can manage a chess game, making moves on a board
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessGame {
+
+    public ChessGame() {
+
+    }
+
+    /**
+     * @return Which team's turn it is
+     */
+    public TeamColor getTeamTurn() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Set's which teams turn it is
+     *
+     * @param team the team whose turn it is
+     */
+    public void setTeamTurn(TeamColor team) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Enum identifying the 2 possible teams in a chess game
+     */
+    public enum TeamColor {
+        WHITE,
+        BLACK
+    }
+
+    /**
+     * Gets a valid moves for a piece at the given location
+     *
+     * @param startPosition the piece to get valid moves for
+     * @return Set of valid moves for requested piece, or null if no piece at
+     * startPosition
+     */
+    public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Makes a move in a chess game
+     *
+     * @param move chess move to preform
+     * @throws InvalidMoveException if move is invalid
+     */
+    public void makeMove(ChessMove move) throws InvalidMoveException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Determines if the given team is in check
+     *
+     * @param teamColor which team to check for check
+     * @return True if the specified team is in check
+     */
+    public boolean isInCheck(TeamColor teamColor) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Determines if the given team is in checkmate
+     *
+     * @param teamColor which team to check for checkmate
+     * @return True if the specified team is in checkmate
+     */
+    public boolean isInCheckmate(TeamColor teamColor) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Determines if the given team is in stalemate, which here is defined as having
+     * no valid moves
+     *
+     * @param teamColor which team to check for stalemate
+     * @return True if the specified team is in stalemate, otherwise false
+     */
+    public boolean isInStalemate(TeamColor teamColor) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Sets this game's chessboard with a given board
+     *
+     * @param board the new board to use
+     */
+    public void setBoard(ChessBoard board) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Gets the current chessboard
+     *
+     * @return the chessboard
+     */
+    public ChessBoard getBoard() {
+        throw new RuntimeException("Not implemented");
+    }
+}
